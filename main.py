9fab0bdf4af0b47a58872ad459b72e0ed6f95221
Brandon Walker <brandonwalker215@gmail.com>
2019-04-18 11:58:45 -0500

    completed basic functionality for newpost and viewpost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import modules for templating, requests, redirects, flask, db stuff
 from flask import Flask, request, redirect, render_template, session, flash
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 
 # create app variable from Flask constructor
 app = Flask(__name__)
@@ -17,25 +17,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:myblog@loc
 app.config['SQLALCHEMY_ECHO'] = True
 
 # create db variable from SQLALchemy constructor (pass in app var)
-db = SQLALchemy(app)
+db = SQLAlchemy(app)
 
 # set secret key for sessions in Flask
 app.secret_key = 'P0uMx81vjH'
 
 # create classes for tables in db (user, post)
+# TODO: add user_id and date to Blog class
+# TODO: add keywords table with post_id relationship (?)
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     keywords = db.Column(db.String(120))
     body = db.Column(db.String(10000))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, keywords, body, owner_id):
+    def __init__(self, title, keywords, body):
         self.title = title
         self.keywords = keywords
         self.body = body
-        self.owner_id = owner_id
 
+'''
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
@@ -45,23 +46,38 @@ class User(db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+'''
 
 # TODO: create handlers/routes for main page, blog post form page, posted blog page
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/blog', methods=['GET', 'POST'])
 def index():
-
-    # pull data from db on old posts, pass to index.html on render call?
+# pull blog post data from db and pass to index.html to display
+    # data should be a list of title/keyword/body tuples or lists
+    # (or just a title: body dictionary?)
 
     return render_template("index.html")
 
-@app.route('/new-post', methods=['GET', 'POST'])
+@app.route('/newpost', methods=['GET', 'POST'])
 def new_post():
-    return render_template("new_post.html")
+    return render_template("newpost.html")
 
-@app.route('/view-post', methods=['GET', 'POST'])
+@app.route('/viewpost', methods=['GET', 'POST'])
 def view_post():
-    return render_template("view_post.html")
+    # collect form data, validate, add to db
+    title = request.form['title']
+    body = request.form['body']
+    keywords = request.form['keywords']
+
+    # TODO: validate and redirect to newpost page with error msgs
+    # add form data to db
+    new_blog = Blog(title, keywords, body)
+    db.session.add(new_blog)
+    db.session.commit()
+
+    # pass form data to viewpost.html and render it
+    return render_template("viewpost.html", title=title, keywords=keywords, 
+    body=body)
 
 # allow for importing without automatic execution of this file
 if __name__ == "__main__":
